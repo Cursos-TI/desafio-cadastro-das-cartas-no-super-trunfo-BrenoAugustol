cb6d36f3050f4235bb099af4b26b77a3d29ad0a1
BrenoAugustol <brenoalp@gmail.com>
2025-03-20 21:25:34 +0000

    feito!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,22 +5,37 @@
          printf("Desafio CartasSuperTrunfo!\n");
         
          printf("Carta1:\n");
-         char estado[50] = "Riodejaneiro";
-         char nome[50] = "Campogrande";
-         char codigo[50] = "A01";
-         int numero = 2000000;
-         int pontoturistico = 40;
-         float area = 500.000;
-         float pib = 20000000;
+         char estado1[50] = "Riodejaneiro";
+         char nome1[50] = "Campogrande";
+         char codigo1[50] = "A01";
+         int numero1 = 2000000;
+         int pontoturistico1 = 40;
+         float area1 = 500000;
+         float pib1 = 20000000;
 
-         printf("Nome do estado é: %s\n", estado);
-         printf("Nome da cidade é: %s\n", nome);
-         printf("Código da cidade é: %s\n", codigo);
-         printf("Número da população da cidade é: %d\n", numero);
-         printf("Quantidade de pontos turísticos da cidade: %d\n", pontoturistico);
-         printf("Qual a área da cidade em m2: %f\n", area);
-         printf("PIB da cidade é: %f\n", pib);
+         printf("Nome do estado é: %s\n", estado1);
+         printf("Nome da cidade é: %s\n", nome1);
+         printf("Código da cidade é: %s\n", codigo1);
+         printf("Número da população da cidade é: %d\n", numero1);
+         printf("Quantidade de pontos turísticos da cidade: %d\n", pontoturistico1);
+         printf("Qual a área da cidade em m2: %.0f\n", area1);
+         printf("PIB da cidade é: %.0f\n", pib1);
+         
+         printf("Carta2:\n");
+         char estado2[50] = "SaoPaulo";
+         char nome2[50] = "Campinas";
+         char codigo2[50] = "B02";
+         int numero2 = 1500000;
+         int pontoturistico2 = 23;
+         float area2 = 200000;
+         float pib2 = 18000000;
 
-        
+         printf("Nome do estado é: %s\n", estado2);
+         printf("Nome da cidade é: %s\n", nome2);
+         printf("Código da cidade é: %s\n", codigo2);
+         printf("Número da população da cidade é: %d\n", numero2);
+         printf("Quantidade de pontos turísticos da cidade: %d\n", pontoturistico2);
+         printf("Qual a área da cidade em m2: %.0f\n", area2);
+         printf("PIB da cidade é: %.0f\n", pib2);
 
      }
